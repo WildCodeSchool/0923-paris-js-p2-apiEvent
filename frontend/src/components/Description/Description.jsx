8f79dbc4883694c107bf2eef7a6d524e55c4041f
RUDY MARTINS <martinsosr@gmail.com>
2023-10-30 16:44:35 +0100

    description page creation

--- frontend/src/components/Description/Description.jsx
+++ frontend/src/components/Description/Description.jsx
@@ -1,11 +1,27 @@
-function Description(props) {
-  const { data } = props;
+import "./description.css";
+
+function Description({data}) {
+ 
   return (
     <>
-      <img src={data.image} alt={data.title_fr} />
-      <h1>{data.title_fr}</h1>
-      <h2>{data.conditions_fr}</h2>
-      <p>{data.longdescription_fr}</p>
+        <div className="alldescritpion">
+      <img src={data.image} alt={data.title_fr} className="pic"/>
+      <div className="desccond">
+      <h3 className="titledescription">{data.title_fr}</h3>
+      <h6 className="conditions">{data.conditions_fr}</h6>
+      </div>
+      <p className="longdescr">{data.longdescription_fr}</p>
+      <hr className="hrtop"></hr>
+      <ul className="descirptiondatelist">
+        <li>{data.date1}</li>
+        <li>{data.date2}</li>
+      </ul>
+      <hr className="hrbot"></hr>
+      <p className="adress">{data.location_address}</p>
+      <button className="booking">book ticket</button>
+      <article className="locations">{data.location_coordinates.lat},{data.location_coordinates.lon} </article>
+      </div>
+      
     </>
   );
 }
