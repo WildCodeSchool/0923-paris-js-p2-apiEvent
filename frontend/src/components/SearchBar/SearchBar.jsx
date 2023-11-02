1c5a2ef2b87fc9a2da48d4bcb48795b4d1efc3c7
LuluMena <lucie.menage@gmail.com>
2023-11-02 13:22:32 +0100

    start searchbar

---
+++ frontend/src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,29 @@
+import "./SearchBar.css";
+
+function SearchBar(props) {
+  const { searchInput, setSearchInput, setCardlist, cardlist } = props;
+
+  function handleSearch(e) {
+    setSearchInput(e.target.value);
+    e.preventDefault();
+    if (searchInput !== "") {
+      const filteredList = cardlist.filter((card) =>
+        card.username.includes(searchInput)
+      );
+      setCardlist(filteredList);
+    }
+  }
+
+  return (
+    <form>
+      <input
+        type="text"
+        placeholder="Musée, théâtre"
+        onChange={(event) => handleSearch(event)}
+        value={searchInput}
+      />
+    </form>
+  );
+}
+
+export default SearchBar;
