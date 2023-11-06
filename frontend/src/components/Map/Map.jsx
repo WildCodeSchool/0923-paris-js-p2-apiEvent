07e8874f93341e70c32cd9503d2fa0f05eaf7010
RUDY MARTINS <martinsosr@gmail.com>
2023-11-06 11:56:43 +0100

    booking function

---
+++ frontend/src/components/Map/Map.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Map({ latitude, longitude }) {
+  return (
+    <p>
+      Latitude: {latitude}, Longitude: {longitude}
+    </p>
+  );
+}
+
+export default Map;
