62a3c42e40dec2f2196ba62c3de779cb6d57ac45
LuluMena <lucie.menage@gmail.com>
2023-11-07 17:01:31 +0100

    fix lint

--- frontend/src/components/SearchBar/SearchBar.jsx
+++ frontend/src/components/SearchBar/SearchBar.jsx
@@ -18,8 +18,9 @@ function SearchBar({ onSearch }) {
           onChange={(e) => setSearch(e.target.value)}
           value={search}
         />
-        <button type="button" onClick={() => handleSearch()}>
-          <Icon icon="charm:search" color="#fafaff" width="40" />
+        <button type="button" onClick={handleSearch()}>
+          {/* <Icon icon="charm:search" color="#fafaff" width="40" /> */}
+          Icon
         </button>
       </form>
     </div>
