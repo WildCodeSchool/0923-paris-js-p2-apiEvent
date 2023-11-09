bac6db72d70e0617c101450cab68b1f1f1be0dab
LuluMena <lucie.menage@gmail.com>
2023-11-09 10:38:24 +0100

    context added

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,16 +1,28 @@
 import SearchBar from "../../components/SearchBar/SearchBar";
+import Description from "../../components/Description/Description";
 import "./Home.css";
+import useAllEventsContext from "../../contexts/AllEvents";
 
-export default function Home({ filterData }) {
+export default function Home() {
+  const { filterData, filteredData } = useAllEventsContext();
   return (
-    <header>
-      <h1 className="logo">EVENRES</h1>
-      <div className="header-name-container">
-        <h2>Discover Events Near You</h2>
-        <div className="searchBar-container">
-          <SearchBar onSearch={filterData} />
+    <>
+      <header>
+        <h1 className="logo">EVENTURES</h1>
+        <div className="header-name-container">
+          <h2>Discover Events Near You</h2>
+          <div className="searchBar-container">
+            <SearchBar filterData={filterData} />
+          </div>
         </div>
+      </header>
+      <div className="cards-display">
+        {filteredData.map((data) => (
+          <div key={data.uid}>
+            <Description data={data} />
+          </div>
+        ))}
       </div>
-    </header>
+    </>
   );
 }
