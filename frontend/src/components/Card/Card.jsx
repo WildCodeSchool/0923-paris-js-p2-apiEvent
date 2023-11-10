e84a8550bbc2a607d5a36df02eceaecc36c84b7c
RUDY MARTINS <martinsosr@gmail.com>
2023-11-10 17:02:21 +0100

    CardList updates

--- frontend/src/components/Card/Card.jsx
+++ frontend/src/components/Card/Card.jsx
@@ -8,13 +8,26 @@ function Card({ obj }) {
     <div className="singleEvent">
       <div className="container">
         <img src={obj.image} alt="img" className="imageEvent" />
-        <Icon icon="ph:heart-fill" color="#E63946" width="15" height="15" />
+        <Icon
+          icon="ph:heart-fill"
+          color="#E63946"
+          width="15"
+          height="15"
+          className="icon-cardList"
+        />
       </div>
-      <h1 className="titleevent">{obj.title_fr}</h1>
-      <ol className="eventdatelist">
-        <li>{new Date(obj.date1).toLocaleDateString()}</li>
-      </ol>
-      <h2 className="conditions">{obj.conditions_fr}</h2>
+      <div className="container-bot">
+        <ol className="eventdatelist">
+          <li>
+            {" "}
+            {new Date(obj.date1).getDate()}{" "}
+            {new Date(obj.date1).toLocaleDateString("default", {
+              month: "short",
+            })}
+          </li>
+        </ol>
+        <h1 className="titleevent">{obj.title_fr}</h1>
+      </div>
     </div>
   );
 }
