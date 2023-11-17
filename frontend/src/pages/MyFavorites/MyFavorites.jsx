abbfc5f7a6cfc8fc54ab471741318c94b08df076
LuluMena <lucie.menage@gmail.com>
2023-11-17 11:01:54 +0100

    final version mobile

--- frontend/src/pages/MyFavorites/MyFavorites.jsx
+++ frontend/src/pages/MyFavorites/MyFavorites.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import useAllEventsContext from "../../contexts/AllEvents";
 import "./MyFavorites.css";
 import Menuburger from "../../components/MenuBurger/MenuBurger";
@@ -14,20 +15,26 @@ function MyFavorites() {
         <div key={event.uid}>
           <div className="singleEvent-fav">
             <div className="container-top-fav">
-              <img src={event.image} alt="img" className="imageEvent-fav" />
+              <Link to={`/Description/${event.uid}`}>
+                <img src={event.image} alt="img" className="imageEvent-fav" />
+              </Link>
             </div>
             <div className="container-bot-fav">
-              <ol className="eventdatelist-fav">
-                <li>
-                  {" "}
-                  {new Date(event.date1).getDate()}{" "}
-                  {new Date(event.date1).toLocaleDateString("default", {
-                    month: "short",
-                  })}
-                </li>
-                <li className="year">{new Date(event.date1).getFullYear()}</li>
-              </ol>
-              <h1 className="titleevent-fav">{event.title_fr}</h1>
+              <Link to={`/Description/${event.uid}`}>
+                <ol className="eventdatelist-fav">
+                  <li>
+                    {" "}
+                    {new Date(event.date1).getDate()}{" "}
+                    {new Date(event.date1).toLocaleDateString("default", {
+                      month: "short",
+                    })}
+                  </li>
+                  <li className="year">
+                    {new Date(event.date1).getFullYear()}
+                  </li>
+                </ol>
+                <h1 className="titleevent-fav">{event.title_fr}</h1>
+              </Link>
             </div>
           </div>
         </div>
